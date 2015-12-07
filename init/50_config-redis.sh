7ad490305a7a1b6ff5c1acb2e8fb9909e98c85df
sparklyballs <sparklyballs@gmail.com>
2015-12-07 16:46:07 +0000

    tweak redis init

--- init/50_config-redis.sh
+++ init/50_config-redis.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 mkdir -p /data/redis /var/log/redis
-chown -R abc:abc /data/redis /var/log/redis
+
+[[ ! -f /config/redis.conf ]] cp /etc/redis/redis.conf /config/redis.conf
+chown -R abc:abc /config/redis.conf /data/redis /var/log/redis
