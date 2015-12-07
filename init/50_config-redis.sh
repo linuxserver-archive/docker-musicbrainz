fdba6dcd50ac2b348d642330bfcc8d4c4683d824
sparklyballs <sparklyballs@gmail.com>
2015-12-07 19:46:22 +0000

    missing && in init

--- init/50_config-redis.sh
+++ init/50_config-redis.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir -p /data/redis /var/log/redis
 
-[[ ! -f /config/redis.conf ]] cp /etc/redis/redis.conf /config/redis.conf
+[[ ! -f /config/redis.conf ]] && cp /etc/redis/redis.conf /config/redis.conf
 chown -R abc:abc /config/redis.conf /data/redis /var/log/redis
