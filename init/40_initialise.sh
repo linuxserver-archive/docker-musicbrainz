04f484233986155fcee45ec08cb6190acfd426c5
sparklyballs <sparklyballs@gmail.com>
2016-01-03 18:49:26 +0000

    move fetching db dump up the order to before initialising postgres, should allow a failed download to be rescuable with deletign databasee folder

--- init/40_initialise.sh
+++ init/40_initialise.sh
@@ -19,6 +19,24 @@ set_listen_addresses() {
 if [ ! -s "$PGDATA/PG_VERSION" ]; then
 chown -R abc:abc "$DATA_ROOT"
 
+dumpver=$(curl -s $URL_ROOT/LATEST)
+DUMP_URL="$URL_ROOT"/"$dumpver"
+DUMP_DEST="$MBDATA"/"$dumpver"
+mkdir -p "$DUMP_DEST"
+chown abc:abc "$DUMP_DEST"
+echo "fetching latest dump from musicbrainz, if this section fails try again later as musicbrainz dump may not be fully uploaded to their site"
+
+# /sbin/setuser abc curl -o "$DUMP_DEST"/MD5SUMS -L -C - "$DUMP_URL"/MD5SUMS
+/sbin/setuser abc curl -o "$DUMP_DEST"/mbdump-cdstubs.tar.bz2 -L -C - "$DUMP_URL"/mbdump-cdstubs.tar.bz2
+/sbin/setuser abc curl -o "$DUMP_DEST"/mbdump-cover-art-archive.tar.bz2 -L -C - "$DUMP_URL"/mbdump-cover-art-archive.tar.bz2
+/sbin/setuser abc curl -o "$DUMP_DEST"/mbdump-derived.tar.bz2 -L -C - "$DUMP_URL"/mbdump-derived.tar.bz2
+/sbin/setuser abc curl -o "$DUMP_DEST"/mbdump-editor.tar.bz2 -L -C - "$DUMP_URL"/mbdump-editor.tar.bz2
+/sbin/setuser abc curl -o "$DUMP_DEST"/mbdump-stats.tar.bz2 -L -C - "$DUMP_URL"/mbdump-stats.tar.bz2
+/sbin/setuser abc curl -o "$DUMP_DEST"/mbdump-wikidocs.tar.bz2 -L -C - "$DUMP_URL"/mbdump-wikidocs.tar.bz2
+/sbin/setuser abc curl -o "$DUMP_DEST"/mbdump.tar.bz2 -L -C - "$DUMP_URL"/mbdump.tar.bz2
+/sbin/setuser abc curl -o "$DUMP_DEST"/mbdump-sitemaps.tar.bz2 -L -C - "$DUMP_URL"/mbdump-sitemaps.tar.bz2
+# /sbin/setuser abc curl -o "$DUMP_DEST"/mbdump-edit.tar.bz2 -L -C - "$DUMP_URL"/mbdump-edit.tar.bz2
+
 echo "initialising empty databases"
 /sbin/setuser abc initdb >/dev/null 2>&1
 echo "completed postgres initialise"
@@ -35,23 +53,6 @@ sleep 5s
 sleep 5s
 echo "BEGINNING INITIAL DATABASE IMPORT ROUTINE, THIS COULD TAKE SEVERAL HOURS AND THE DOCKER MAY LOOK UNRESPONSIVE"
 echo "DO NOT STOP DOCKER UNTIL IT IS COMPLETED"
-
-dumpver=$(curl -s $URL_ROOT/LATEST)
-DUMP_URL="$URL_ROOT"/"$dumpver"
-DUMP_DEST="$MBDATA"/"$dumpver"
-mkdir -p "$DUMP_DEST"
-chown abc:abc "$DUMP_DEST"
-
-# /sbin/setuser abc curl -o "$DUMP_DEST"/MD5SUMS -L -C - "$DUMP_URL"/MD5SUMS
-/sbin/setuser abc curl -o "$DUMP_DEST"/mbdump-cdstubs.tar.bz2 -L -C - "$DUMP_URL"/mbdump-cdstubs.tar.bz2
-/sbin/setuser abc curl -o "$DUMP_DEST"/mbdump-cover-art-archive.tar.bz2 -L -C - "$DUMP_URL"/mbdump-cover-art-archive.tar.bz2
-/sbin/setuser abc curl -o "$DUMP_DEST"/mbdump-derived.tar.bz2 -L -C - "$DUMP_URL"/mbdump-derived.tar.bz2
-/sbin/setuser abc curl -o "$DUMP_DEST"/mbdump-editor.tar.bz2 -L -C - "$DUMP_URL"/mbdump-editor.tar.bz2
-/sbin/setuser abc curl -o "$DUMP_DEST"/mbdump-stats.tar.bz2 -L -C - "$DUMP_URL"/mbdump-stats.tar.bz2
-/sbin/setuser abc curl -o "$DUMP_DEST"/mbdump-wikidocs.tar.bz2 -L -C - "$DUMP_URL"/mbdump-wikidocs.tar.bz2
-/sbin/setuser abc curl -o "$DUMP_DEST"/mbdump.tar.bz2 -L -C - "$DUMP_URL"/mbdump.tar.bz2
-/sbin/setuser abc curl -o "$DUMP_DEST"/mbdump-sitemaps.tar.bz2 -L -C - "$DUMP_URL"/mbdump-sitemaps.tar.bz2
-# /sbin/setuser abc curl -o "$DUMP_DEST"/mbdump-edit.tar.bz2 -L -C - "$DUMP_URL"/mbdump-edit.tar.bz2
 
 cd /app/musicbrainz
 /sbin/setuser abc ./admin/InitDb.pl --createdb --import "$DUMP_DEST"/mbdump*.tar.bz2 --tmp-dir "$DUMP_DEST" --echo
