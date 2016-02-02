c8440f80b74ab931d818bfa55ae8cdf753159496
sparklyballs <sparklyballs@gmail.com>
2016-02-02 18:12:54 +0000

    shellcheck fixing

--- init/005_set_time.sh
+++ init/005_set_time.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [[ $(cat /etc/timezone) != $TZ ]] ; then
+if [[ $(cat /etc/timezone) != "$TZ" ]] ; then
   echo "$TZ" > /etc/timezone
  exec  dpkg-reconfigure -f noninteractive tzdata
 fi
